84954c5c46b98e7f487349b87da756ecef73adad
Kerim Deveci <beyzaybikerim@gmail.com>
2021-01-16 00:42:12 +0300

    argument parsing added

--- translate.py
+++ translate.py
@@ -3,6 +3,8 @@ import urllib.parse
 import re
 import html
 import os
+import codecs
+import argparse
 
 agent = {'User-Agent':
          "Mozilla/4.0 (\
@@ -34,6 +36,29 @@ LANGS = [  # tuple array ( [FOR_LPROJ_FOLDER_NAME] , [FOR_GOOGLE_TRANSLATE_QUERY
 ]
 
 
+def detect_encoding(full_file_path):
+    with open(full_file_path, 'rb') as file:
+        s = file.readline()
+    if s.startswith(codecs.BOM_UTF16_BE):
+        return 'utf-16-be'
+    if s.startswith(codecs.BOM_UTF16_LE):
+        return 'utf-16-le'
+    if s.startswith(codecs.BOM_UTF32_BE):
+        return 'utf-32-be'
+    if s.startswith(codecs.BOM_UTF32_LE):
+        return 'utf-32-le'
+    if s.startswith(codecs.BOM_UTF8):
+        return 'utf-8'
+    m = re.match(br'\s*<\?xml\b.*\bencoding="([^"]+)"', s)
+    if m:
+        return m.group(1).decode()
+    m = re.match(br"\s*<\?xml\b.*\bencoding='([^']+)'", s)
+    if m:
+        return m.group(1).decode()
+    # No encoding found -- what should the default be?
+    return 'utf-8'
+
+
 def translate(to_translate, to_language="auto", from_language="auto"):
     """Returns the translation using google translate
     you must shortcut the language you define
@@ -63,7 +88,7 @@ def print_to_output():
     array = []
     for lang in sorted(LANGS):
         print("______________________")
-        print(f" {lang[0]}")
+        print(f"translating to {lang[0]}")
         array2 = []
         for item in TRANSLATE_ARRAY:
             translation = translate(item, lang[1], "en")
@@ -71,8 +96,7 @@ def print_to_output():
             print(new_format)
             array2.append(new_format)
         array.append(array2)
-        print("______________________")
-        print("\n")
+        print("______________________\n")
         return array
 
 
@@ -85,17 +109,18 @@ def translate_to_language(language):
         new_format = (f"\"{item}\" = \"{translation}\";")
         print(new_format)
         array2.append(new_format)
-    print("______________________\n")
     return array2
 
 
 def does_key_exist_in_file(translate_result, full_file_path):
     """Returns boolean if the key existed in the file """
-    with open(full_file_path, 'r', encoding='utf-16') as file:
-        # file.seek(0)
+    encoding = detect_encoding(full_file_path)
+    with open(full_file_path, 'r', encoding=encoding) as file:
+        file.seek(0)
         lines = file.read()
         key = translate_result.split("=")[0]
         if key in lines:
+            print(f"the key: {key} is already exists skipping writing")
             return True
         return False
 
@@ -104,20 +129,45 @@ def write_results_in_file(translate_results):
     """ writes the translation results to Localizable.string"""
     file_path = FOLDER_PATH + '/' + language_folder + '/' + "Localizable.strings"
     try:
-        with open(file_path, 'a+', encoding='utf-16') as file:
+        encoding = detect_encoding(file_path)
+        with open(file_path, 'a+', encoding=encoding) as file:
+            print("\n")
             for t_result in translate_results:
                 if does_key_exist_in_file(t_result, file_path) == True:
                     continue
                 file.write(f'{t_result}\n')
+                print(f"Writing {t_result} to file {file_path} ...")
     except IOError:
         print('File Write not Success')
 
 
 if __name__ == "__main__":
+
+    dir = os.getcwd()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i',
+        '--input_dir',
+        help='Full Output Path eg: Users/[username]/Desktop/StickerCategories')
+
+    parser.add_argument("-l", "--list", nargs="+",
+                        default=["Show Offer", "Continue"])
+
+    args = parser.parse_args()
+
+    if args.input_dir:
+        FOLDER_PATH = args.input_dir
+    else:
+        FOLDER_PATH = dir
+
+    if args.list:
+        TRANSLATE_ARRAY = args.list
+
     for language_folder in os.listdir(FOLDER_PATH):
         for script_lang in sorted(LANGS):
             language_name = os.path.splitext(language_folder)[0]
             if language_name in script_lang[0]:
-                print(language_name)
+                print(f"translating to {language_name}  ==> \n")
                 values = translate_to_language(script_lang[1])
                 write_results_in_file(values)
+                print("______________________\n")
